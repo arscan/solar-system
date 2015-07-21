6fb21c7d906fca2d4255a26e579986becfea54cb
Rob Scanlon <robscanlon@gmail.com>
2015-07-21 19:20:20 -0400

    finalize the initial setup

--- dist/solar-system.js
+++ dist/solar-system.js
@@ -1,12 +1,15 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var _planet = require("planet");
+var _planet = require("./planet");
 
 var _planet2 = _interopRequireDefault(_planet);
 
 document.write(new _planet2["default"]("Hi"));
+
+},{"./planet":2}],2:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -36,4 +39,8 @@ var Planet = (function () {
 
 exports["default"] = Planet;
 module.exports = exports["default"];
+
+},{}]},{},[1])
+
+
 //# sourceMappingURL=solar-system.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,39 @@
-/* File: gulpfile.js */
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
 
-// grab our packages
-var gulp   = require("gulp"),
-    eslint = require("gulp-eslint"),
-    babel = require("gulp-babel"),
-    sourcemaps = require("gulp-sourcemaps"),
-    concat = require("gulp-concat");
+function compile(watch) {
+  var bundler = watchify(browserify('./src/main.js', { debug: true }).transform(babel));
 
-// define the default task and add the watch task to it
-gulp.task("default", ["watch"]);
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('solar-system.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./dist'));
+  }
 
-// configure the jshint task
-gulp.task("lint", function() {
-  return gulp.src("src/**/*.js")
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failOnError());
-});
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
 
-gulp.task("babel", function () {
-  return gulp.src("src/**/*.js")
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(concat("solar-system.js"))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist"));
-});
+  rebundle();
+}
 
-// configure which files to watch and what tasks to use on file changes
-gulp.task("watch", function() {
-  gulp.watch("src/*.js", ["lint", "babel"]);
-});
+function watch() {
+  return compile(true);
+};
 
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
 
+gulp.task('default', ['watch']);
